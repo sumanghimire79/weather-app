b2351bb2ed2cd8c266aed62d7c4a06688d77cd60
sumanghimire79 <sumanghimire79@yahoo.com>
2022-10-19 22:38:30 +0200

    weather app antoher version submitted

--- js3w1-weatherApi.js
+++ js3w1-weatherApi.js
@@ -1,128 +1,167 @@
-// Weather app
+//Create your own json file
 
-// weather api: https://api.openweathermap.org/data/2.5/weather?q=copenhagen&appid=a7c9cec0afea0a25677e56ca91474164
-//key = a7c9cec0afea0a25677e56ca91474164
-let latitude ;
-let longitude ;
+[
+  {
+    artist: 'Bob Marley',
+    albums: ['burnin', 'catch a fire', 'soul revolution', 'natty dread'],
+    hitsongs: [
+      { title: 'buffalo soldier', length: '6:15', year: 1975 },
+      { title: 'no woman no cry', length: '5:47', year: 1989 },
+    ],
+    genre: 'reggae',
+  },
+  {
+    artist: 'coldplay',
+    albums: ['parachuets', 'xny', 'ghost stories'],
+    hitsongs: [
+      { title: 'yellow', length: '5:15', year: 2000 },
+      { title: 'viva la vida', length: '5:40', year: 2005 },
+    ],
+    genre: 'rock',
+  },
+];
 
-let weatherUl = document.querySelector('#weatherUl');
-let weatherMoreUl= document.querySelector('#weatherMoreUl');
-let searchInput = document.querySelector('#weatherSearchByCity');
-let button = document.querySelector('#weatherButton');
+// Find a cool api
+//aniapi.com/docs/
+https: [
+  {
+    status_code: 200, // number
+    message: 'Anime found', // string
+    data: {
+      // object
+      anilist_id: 1,
+      mal_id: 1,
+      format: 0,
+      'status:': 0,
+      titles: {
+        //object inside object
+        en: 'Cowboy Bebop',
+        jp: 'カウボーイビバップ',
+      },
+      // ...
+      id: 0,
+    },
+    version: '1', // The response API version
+  },
+];
 
-button.addEventListener('click', () => navigator.geolocation.getCurrentPosition(cityName));
+//Weather app //be9e1928280950bedf8ff42caa2aa649
+function getClassName() {
+  const cityInput = document.querySelector('.search-bar');
+  const btnToSearchByCity = document.querySelector('.btnSearch');
+  const cityName = document.querySelector('.cityName');
+  const cityTemp = document.querySelector('.temp');
+  const feelsLike = document.querySelector('.feels-like');
+  const cityIcon = document.querySelector('.icon');
+  const windSpeed = document.querySelector('.windSpeed');
+  const cloudy = document.querySelector('.cloudy');
+  const sunrise = document.querySelector('.sunrise');
+  const sunset = document.querySelector('.sunset');
+  const description = document.querySelector('.description');
+  const errorDisplay = document.querySelector('.error-display');
 
-//get weather by city name
-function cityName(city) {
-    city = searchInput.value;
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a7c9cec0afea0a25677e56ca91474164`)
-        .then(response => response.json())
-        .then(weatherDisplay);
+  return {
+    cityInput,
+    btnToSearchByCity,
+    cityName,
+    cityTemp,
+    feelsLike,
+    cityIcon,
+    windSpeed,
+    cloudy,
+    sunrise,
+    sunset,
+    description,
+    errorDisplay,
+  };
 }
+let weatherApi;
 
-const getCurrentlocationWeatherButton = document.querySelector('#getCurrentlocationWeather');
-getCurrentlocationWeatherButton.addEventListener('click', () => window.navigator.geolocation.getCurrentPosition(currentPosition));
+const getClass = getClassName();
 
-//get weather by current position
-function currentPosition(position) {
-     latitude = position.coords.latitude;
-     longitude = position.coords.longitude;
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a7c9cec0afea0a25677e56ca91474164`)
-        .then(response => response.json())
-        .then(weatherDisplay);
+//local storage start
+const city = localStorage.getItem('myCity');
+if (city) {
+  weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=be9e1928280950bedf8ff42caa2aa649&units=metric`;
+  renderApiData(weatherApi);
 }
+//local storage end
 
-// callback function 
-function weatherDisplay(weatherData) {
+// geolocation api start
+const btnToKnowCurrentPosition = document.querySelector('.currentPosition');
 
-    // clears the list for fresh/new rendered list
-    weatherUl.innerHTML = ""; 
-    weatherMoreUl.innerHTML= "";
-    console.log(weatherData);
+// searching weather data using current location of the user
+btnToKnowCurrentPosition.addEventListener('click', () => {
+  getClass.errorDisplay.innerHTML = '';
+  function success(pos) {
+    let crd = pos.coords;
+    weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=be9e1928280950bedf8ff42caa2aa649&units=metric`;
+    renderApiData(weatherApi);
+  }
 
-    // get city Name 
-    const city = weatherData.name;
-    const nam = document.querySelector('#cityName');
-    nam.innerText = `${city} Region`;
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  navigator.geolocation.getCurrentPosition(success, error);
+});
 
-    // get temperature
-    const temeratureInDegree = ((weatherData.main.temp) - 273.15).toFixed() + '°';
+// geolocation api end
 
-    // get feelslike
-    const feelsLike = ((weatherData.main.feels_like) - 273.15).toFixed() + '°';
+// searching weather data using city name
+getClass.btnToSearchByCity.addEventListener('click', () => {
+  getClass.errorDisplay.innerHTML = '';
+  weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${getClass.cityInput.value}&appid=be9e1928280950bedf8ff42caa2aa649&units=metric`;
+  renderApiData(weatherApi);
+});
 
-    // get windspeed
-    const speed = weatherData.wind.speed;
+getClass.cityInput.addEventListener('change', () => {
+  getClass.errorDisplay.innerHTML = '';
+  weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${getClass.cityInput.value}&appid=be9e1928280950bedf8ff42caa2aa649&units=metric`;
+  renderApiData(weatherApi);
+});
 
-    // get weather situation and icon
-    let weatherSituation;
-    let weatherIconCode;
-    let iconimg;
-    const situation = weatherData.weather.map(weatherArray => {
-        weatherSituation = weatherArray.description;
-        weatherIconCode = weatherArray.icon;
-        iconimg = document.querySelector('#img1');
-        iconimg.src = `http://openweathermap.org/img/wn/${weatherIconCode}.png`;
-        iconimg.alt = `icon img`;
-        // iconimg.href = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a7c9cec0afea0a25677e56ca91474164`; 
-    });
+function renderApiData(weatherApi) {
+  fetch(weatherApi)
+    .then((res) => res.json())
+    .then((yesNoData) => {
+      if (yesNoData.name) {
+        getClass.cityName.innerHTML = `Weather in <strong>${yesNoData.name}</strong>`;
+        getClass.cityTemp.innerHTML = yesNoData.main.temp + ' °C';
+        getClass.feelsLike.innerHTML = `Feels like ${yesNoData.main.feels_like} °C`;
+        getClass.description.innerHTML = yesNoData.weather[0].description;
+        const iconDisplay = yesNoData.weather[0].icon;
+        /*cityIcon.innerHTML=`<img src="http://openweathermap.org/img/w/${iconDisplay}.png" alt="weather icon">` */
+        getClass.cityIcon.setAttribute(
+          'src',
+          `http://openweathermap.org/img/w/${iconDisplay}.png`,
+        );
+        getClass.windSpeed.innerHTML = `Wind Speed: ${yesNoData.wind.speed} km/h`;
+        getClass.cloudy.innerHTML = `Cloud in the sky:  ${yesNoData.clouds.all} %`;
 
-    // get sunrise 
-    const timestrSunrise = weatherData.sys.sunrise;
-    let date = new Date(timestrSunrise * 1000);
-    const sunRise = date.toLocaleTimeString();
+        // converting time to local time
+        const sunriseDate = yesNoData.sys.sunrise;
+        const sunriseTime = new Date(sunriseDate * 1000);
+        const sunsetTime = new Date(yesNoData.sys.sunset * 1000);
 
-    // get sunset
-    const timestrSunSet = weatherData.sys.sunset;
-    date = new Date(timestrSunSet * 1000);
-    const sunSet = date.toLocaleTimeString();
+        const timeSunrise = sunriseTime.toLocaleTimeString();
+        const timeSunset = sunsetTime.toLocaleTimeString();
+        getClass.sunrise.innerHTML = 'Sunrise: ' + timeSunrise;
+        getClass.sunset.innerHTML = `Sunset:  ${timeSunset}`;
 
-    // get lattitude
-    // const cityLattitude = weatherData.coord.lat;
-    // const cityLongitude = weatherData.coord.lon;
+        // show city on a map
+        const latitude = yesNoData.coord.lat;
+        const longitude = yesNoData.coord.lon;
+        const locationToLink = document.querySelector('#location-link');
+        locationToLink.href = `https://maps.google.com/maps?q=${latitude},${longitude}&hl=es;z=14&amp;output=embed`;
+        document.body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${yesNoData.name})`;
 
-    // get latitude, longitude and then append link to the map
-    const weatherCityMap = document.querySelector('#weatherCityMap');
-    const hrefs = `https://maps.google.com/maps?q=${latitude},${longitude}&hl=es;z=14&amp;output=embed`
-    weatherCityMap.href = hrefs;
-    weatherCityMap.innerText = `Lattidude: ${latitude.toFixed() + '°'}, Longitude ${longitude.toFixed() + '°'} `
-    const linktoMap = document.querySelector('#linktoMap');
-    linktoMap.href = hrefs;
-    document.querySelector('#iframe1').href = hrefs;// refused to display x-option in same origin?? could not fixed this ??
+        localStorage.setItem('myCity', yesNoData.name); // storing the last visited city
+      } else {
+        //alert("not a valid city");
 
-    // created object from gathered info
-    const weatherObject = {
-        City: city,
-        Temperature: temeratureInDegree,
-        'Feels Like': feelsLike,
-        'Weather Condition': weatherSituation
-        // looks: iconimg, this did not work??
-    };
-   
-    // loop throught the boject and append the info to the html list
-    let innerText = "";
-    for (let keys in weatherObject) {
-        innerText = keys + ' : ' + weatherObject[keys];
-        const weatherLi = document.createElement('li');
-        weatherLi.innerHTML = innerText;
-        weatherUl.appendChild(weatherLi);
-    };
-
-    const weatherObjectMore = {
-        'Wind Speed': speed.toFixed() + ' km/h.',
-        Sunrise: sunRise,
-        Sunset: sunSet
-    };
-    let innerMoreText = "";
-    for (let keys in weatherObjectMore) {
-        innerMoreText = keys + ' : ' + weatherObjectMore[keys];
-        const weatherMoreLi = document.createElement('li');
-        weatherMoreLi.innerHTML = innerMoreText;
-        weatherMoreUl.appendChild(weatherMoreLi);
-    };
-console.log(innerMoreText);
-    //
-
-} //end of call back function
-
-
+        const p = document.createElement('p');
+        p.innerHTML = '<strong>NOT VALID<strong>';
+        getClass.errorDisplay.appendChild(p);
+      }
+    }); // end of fetch
+} // end of
